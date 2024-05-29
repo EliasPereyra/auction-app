3afcfb9197d23a1d1b0ce3fc1e930f0c910c146e
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-05-29 10:15:06 -0300

    Create a db file

---
+++ src/db/db.ts
@@ -0,0 +1,26 @@
+import { PostgresJsDatabase, drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+import { env } from "../env";
+
+import * as schema from "./schema";
+
+declare global {
+  var db: PostgresJsDatabase<typeof schema> | undefined;
+}
+
+let db: PostgresJsDatabase<typeof schema>;
+let pg: ReturnType<typeof postgres>;
+
+if (env.NODE_ENV === "production") {
+  pg = postgres(env.DATABASE_URL);
+  db = drizzle(pg, { schema });
+} else {
+  if (!global.db) {
+    pg = postgres(env.DATABASE_URL);
+    global.db = drizzle(pg, { schema });
+  }
+  db = global.db;
+}
+
+export { db, pg };
